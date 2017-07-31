a805fc3da9520cb66e7557b9cb884ff1e377fe26
gdhatric <gdhatric@deloitte.com>
2017-07-31 15:24:56 +0200

    Pronunciation First Commit

---
+++ Pronunciation/com/dhatric/video/VideoPronunciation.py
@@ -0,0 +1,15 @@
+from moviepy.editor import *
+import AudioPronunciation
+
+screensize = (720,460)
+output_audio_directory='../../../output/video/'
+
+def createVideo(word):
+    audio_file = AudioPronunciation.createAudio(word)
+    txt_word = TextClip(word,fontsize=60,color='white',font='Amiri-Bold')
+    txt_word = txt_word.set_pos('center').set_duration(10)
+    video = CompositeVideoClip([txt_word],size=screensize)
+    video.write_videofile(output_audio_directory+word+".mp4",fps=60,codec="mpeg4",audio=audio_file)
+    
+    
+createVideo("welcome")    
